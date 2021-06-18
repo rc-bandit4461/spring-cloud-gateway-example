27f73d6135d4d76241753214a68f852296bf0c21
Baymane <ayman.boubleh@gmail.com>
2021-06-18 21:31:25 +0100

    add security

--- first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceApplication.java
@@ -3,6 +3,12 @@ package com.example.firstservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +23,28 @@ public class FirstServiceApplication {
 }
 
 @RestController
-class HelloWorldController{
+class HelloWorldController {
 	@Value("${server.port}")
 	private String port;
 
-	@GetMapping("/hello")
-	public String hello(){
-			return "hello from port: " + port;
+	@GetMapping("/public")
+	public String publicResource() {
+		return "Private resource: hello from port: " + port;
+	}
+
+	@GetMapping("/private")
+	public String privateResource() {
+		return "Private resource: hello from port: " + port;
+	}
+}
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class Oauth2SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorize -> authorize.antMatchers("/public**").permitAll().anyRequest().authenticated())
+				.oauth2ResourceServer().jwt();
 	}
 }
--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -26,5 +31,29 @@ class GatewayConfig{
 
 								.uri("lb://FIRST-SERVICE/"))
 								.build();
+	}
+}
+
+@Configuration
+@EnableWebFluxSecurity
+class GatewaySecurityConfig {
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.csrf().disable().authorizeExchange().pathMatchers("/public**","/first/public**").permitAll().anyExchange().authenticated().and().oauth2Login().and()
+				.oauth2ResourceServer().jwt();
+		return http.build();
+	}
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/public")
+	public String publicResource() {
+		return "this is a public resource";
+	}
+
+	@GetMapping("/private")
+	public String privateResource() {
+		return "This is a private resource";
 	}
 }
